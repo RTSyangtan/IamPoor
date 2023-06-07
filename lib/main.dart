8a185d1197b569098a50247b4ea114114b9eea3c
RTSyangtan <rnrtamang@gmail.com>
2023-06-07 07:01:17 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text("I am Poor "),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body:Center(
+        child: Image(
+          image: AssetImage("images/poor.png"),
+        ),
+      )
+    ),
+  ),
+  );
+}
